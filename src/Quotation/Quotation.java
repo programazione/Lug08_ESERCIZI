f976ef07793a42018bf9e2ac077ba082e074911f
Deborah <66A@FedericaP>
2026-07-13 13:11:29 +0200

    Esercizio Quotation

---
+++ src/Quotation/Quotation.java
@@ -0,0 +1,16 @@
+package Quotation;
+
+public class Quotation {
+	public String quote = "Sono arrivato!"; // è una variabile globale
+	public String nome = "Deborah";
+	public String cognome = "Sousa";
+	public void display() { // display è il metodo
+
+	System.out.println(quote);
+	System.out.println(nome);
+	System.out.println(cognome);
+
+	}
+	
+
+}
---
+++ src/Quotation/quotationTest.java
@@ -0,0 +1,45 @@
+package Quotation;
+
+public class quotationTest {
+
+	public static void main(String[] args) {
+		Quotation myQuotation = new Quotation(); // istanzio,,,
+		myQuotation.quote="So programmare in Java";
+		myQuotation.display(); // invoco il metodo dell'istanza
+		
+		myQuotation.nome="Deborah";
+		
+		System.out.println("DDDDDD		");
+    	System.out.println("DDDDDDD		");
+    	System.out.println("DD    DD	");
+    	System.out.println("DD     DD	");
+    	System.out.println("DD      DD	");
+    	System.out.println("DD      DD	");
+    	System.out.println("DD     DD	");
+    	System.out.println("DD    DD	");
+    	System.out.println("DDDDDDD		");
+    	System.out.println("DDDDDD		");
+    	System.out.println();
+    	
+    	
+    	
+    	myQuotation.cognome="Sousa";
+    	
+		System.out.println("DDDDDD       SSSSSSSS"    );
+    	System.out.println("DDDDDDD     SSSSSSSSSS"   );
+    	System.out.println("DD    DD   SS        SS"  );
+    	System.out.println("DD     DD  SS"            );
+    	System.out.println("DD      DD  SSSSSSSS"     );
+    	System.out.println("DD      DD   SSSSSSSS"    );
+    	System.out.println("DD     DD            SS"  );
+    	System.out.println("DD    DD    SS        SS" );
+    	System.out.println("DDDDDDD      SSSSSSSSSS"  );
+    	System.out.println("DDDDDD        SSSSSSSS"   );
+
+		
+		
+	}
+	
+	
+
+}
