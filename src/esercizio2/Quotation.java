27fab108b252758dce64939619fa3135d7964c3b
Deborah <66A@192.168.1.59>
2026-07-18 14:11:41 +0200

    ESERCIZIO

---
+++ src/esercizio2/Quotation.java
@@ -0,0 +1,13 @@
+package esercizio2;
+
+	public class Quotation {
+
+		public String quote = "Sono arrivato!"; 
+		
+		public void display() {
+
+			System.out.println(quote);
+
+		}
+
+}
---
+++ src/esercizio2/QuotationTest.java
@@ -0,0 +1,13 @@
+package esercizio2;
+
+public class QuotationTest {
+
+	public static void main (String args[]) {
+
+		Quotation myQuotation = new Quotation(); 
+		myQuotation.quote = "So programmare in Java!";
+		myQuotation.display();
+
+	}
+
+}
